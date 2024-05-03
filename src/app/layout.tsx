07f5a7bacf9ceac692456c06b02940d1cccfb4a0
Edvin Sjögren <edvin.sjogren@gmail.com>
2024-05-03 13:20:18 +0200

    fixup! feat: update root layout with font and QueryParamProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import NextAdapterApp from 'next-query-params/app';
 import { QueryParamProvider } from 'use-query-params';
 import './globals.css';
 import { sanfrancisco } from '../utils/font_utils';
+import { Suspense } from 'react';
 
 export default function RootLayout({
   children,
@@ -15,9 +16,11 @@ export default function RootLayout({
         className={`${sanfrancisco.variable} bg-off-white font-sanfrancisco`}
       >
         <main className="relative mx-auto flex w-full max-w-screen-xl flex-col px-6 py-10">
-          <QueryParamProvider adapter={NextAdapterApp}>
-            {children}
-          </QueryParamProvider>
+          <Suspense>
+            <QueryParamProvider adapter={NextAdapterApp}>
+              {children}
+            </QueryParamProvider>
+          </Suspense>
         </main>
       </body>
     </html>
