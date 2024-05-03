f7660a8984b40df738f1b96448fe1fea092232ef
Edvin Sjögren <edvin.sjogren@gmail.com>
2024-05-03 12:12:45 +0200

    feat: add fonts

---
+++ src/utils/font_utils.ts
@@ -0,0 +1,15 @@
+import local from 'next/font/local';
+
+export const sanfrancisco = local({
+  src: [
+    {
+      path: '../../public/fonts/SF-Pro.ttf',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/SF-Pro-Display-Bold.otf',
+      weight: '760',
+    },
+  ],
+  variable: '--font-sanfrancisco',
+});
