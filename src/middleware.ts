141c564e9a04bcfa3f2b2ecf510187d2bf05df6b
Edvin Sjögren <edvin.sjogren@gmail.com>
2024-05-03 15:42:38 +0200

    fix(middlware): remove redundant cookie settings for safari compatibility

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,6 @@ export function middleware(request: NextRequest) {
     response.cookies.set('splash', 'true', {
       path: '/',
       maxAge: 60 * 60 * 24 * 7,
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
     });
   }
 
