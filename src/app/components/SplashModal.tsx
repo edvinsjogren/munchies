05be1a34ce4ffc07a34f8c2b12dd2335079dcb10
Edvin Sjögren <edvin.sjogren@gmail.com>
2024-05-12 22:23:50 +0200

    fix: initial visit tracking by replacing cookies with local storage
    Replaced cookies with local storage to address the issue of cookies not being recognized on initial page load. This change ensures reliable tracking of user visits without the need for a page refresh, enhancing user experience and reducing frontend complexity.

--- src/app/components/SplashModal.tsx
+++ src/app/components/SplashModal.tsx
@@ -1,31 +1,30 @@
 'use client';
-import React, { useState } from 'react';
 import Logo from './Logo';
 
-function SplashModal() {
-  const [modalOpen, setModalOpen] = useState(true);
+interface SplashModalProps {
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
+function SplashModal({ setModalOpen }: SplashModalProps) {
   return (
-    modalOpen && (
-      <div className="fixed inset-0 z-50 h-dvh w-dvw bg-green px-6 py-10 text-white ">
-        <div className="flex h-full flex-col justify-between gap-8 lg:mx-auto lg:w-max lg:justify-center">
-          <Logo color="white" />
-          <div className="w-60 lg:w-full">
-            <h1 className="mb-4 text-5xl font-[760]">Treat yourself.</h1>
-            <p className="text-title">
-              Find the best restaurants in your city and get it delivered to
-              your place!
-            </p>
-          </div>
-          <button
-            onClick={() => setModalOpen(false)}
-            className="mx-auto w-full rounded-lg border-small border-white py-5 text-center shadow-box-shadows transition hover:bg-white hover:text-green"
-          >
-            Continue
-          </button>
+    <div className="fixed inset-0 z-50 h-dvh w-dvw bg-green px-6 py-10 text-white ">
+      <div className="flex h-full flex-col justify-between gap-8 lg:mx-auto lg:w-max lg:justify-center">
+        <Logo color="white" />
+        <div className="w-60 lg:w-full">
+          <h1 className="mb-4 text-5xl font-[760]">Treat yourself.</h1>
+          <p className="text-title">
+            Find the best restaurants in your city and get it delivered to your
+            place!
+          </p>
         </div>
+        <button
+          onClick={() => setModalOpen(false)}
+          className="mx-auto w-full rounded-lg border-small border-white py-5 text-center shadow-box-shadows transition hover:bg-white hover:text-green"
+        >
+          Continue
+        </button>
       </div>
-    )
+    </div>
   );
 }
 
